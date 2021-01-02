169dee9c6fd7c6b8acc8f172834e21babcadb1b0
tcole8899 <tcole8899@gmail.com>
2021-01-02 18:12:37 -0500

    fix

---
+++ portfolio/build/precache-manifest.22d3119330fec05052d43cb23229beb3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49687a43877f76b38cada6a26dbaf0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9cdc74e2649239982b7",
+    "url": "/static/css/main.1850ff41.chunk.css"
+  },
+  {
+    "revision": "fcefaddfc9787b5959d5",
+    "url": "/static/js/2.02a73d77.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.02a73d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cdc74e2649239982b7",
+    "url": "/static/js/main.f30ae82d.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/GitHub-logo.76d344f0.svg"
+  },
+  {
+    "revision": "9f1e3adb9cec7484c8389b621c6cd44c",
+    "url": "/static/media/linkedin.9f1e3adb.png"
+  },
+  {
+    "revision": "28d5d2e297eafa8621dcea437eca00f8",
+    "url": "/static/media/urilogo.28d5d2e2.png"
+  },
+  {
+    "revision": "6fce31c95747a88a485c48343c2093eb",
+    "url": "/static/media/x.6fce31c9.png"
+  }
+]);
